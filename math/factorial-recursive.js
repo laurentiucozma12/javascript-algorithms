3be8966148522c262023192f41b6754f54125bbe
Lau <laurentiucozma12@gmail.com>
2024-02-22 11:30:16 +0200

    factorial recursive

--- math/factorial-recursive.js
+++ math/factorial-recursive.js
@@ -1,9 +1,9 @@
 function recursiveFactorial(n) {
     if (n === 0) {
-        return 1
+        return 1;
     }
-    
-    return n * recursiveFactorial(n - 1)
+
+    return n * recursiveFactorial(n - 1);
 }
 
 console.log(recursiveFactorial(0)) // 1
