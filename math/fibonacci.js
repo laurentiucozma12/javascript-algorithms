dc683856d277e34680bb397a09000e8a62b9e04d
Lau <laurentiucozma12@gmail.com>
2024-02-22 11:29:07 +0200

    factorial

--- math/fibonacci.js
+++ math/fibonacci.js
@@ -1,11 +1,11 @@
 function fibonacci(n) {
-    const fib = [0, 1]
-    
+    const fib = [0, 1];
+
     for (let i = 2; i <= n; i++) {
-      fib[i] = fib[i - 1] + fib[i - 2]
+      fib[i] = fib[i - 1] + fib[i - 2];
     }
 
-    return fib
+    return fib;
 }
   
 console.log(fibonacci(2)) // [0,1]
