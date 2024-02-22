9912fb9ded6eafb62211156ec16789a4da479785
Lau <laurentiucozma12@gmail.com>
2024-02-22 11:41:50 +0200

    tower of hanoi

--- misc/tower-of-hanoi.js
+++ misc/tower-of-hanoi.js
@@ -3,7 +3,7 @@ function towerOfHanoi(n, fromRod, toRod, usingRod) {
       console.log(`Move disk 1 from ${fromRod} to ${toRod}`);
       return;
     }
-    
+     
     towerOfHanoi(n - 1, fromRod, usingRod, toRod);
     console.log(`Move disk ${n} from ${fromRod} to ${toRod}`);
     towerOfHanoi(n - 1, usingRod, toRod, fromRod);
